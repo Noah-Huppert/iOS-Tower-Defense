abb6a6135afbad230635baa22036ceef769626f1
Noah Huppert <noahhuppert@gmail.com>
2015-12-11 14:26:43 -0500

    Added boilerplate Spritekit game

---
+++ iOS-Tower-Defence/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  iOS-Tower-Defence
+//
+//  Created by block7 on 12/11/15.
+//  Copyright (c) 2015 block7. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
